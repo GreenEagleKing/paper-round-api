9c75e19b195a3f79ae3e83729d9c5dda59d20e0f
Ben King <benking1293@gmail.com>
2022-09-11 20:27:56 +0100

    "FIxed EJS error and created markDelivered"

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,19 @@
 const deleteBtn = document.querySelectorAll('.fa-trash')
+const house = document.querySelectorAll('.house span')
+const paperDelivered = document.querySelectorAll('.house span.delivered')
 
 
 Array.from(deleteBtn).forEach((element) => {
     element.addEventListener('click', deleteItem)
 })
+
+Array.from(house).forEach((element) => {
+    element.addEventListener('click', markDelivered)
+})
+
+// Array.from(paperDelivered).forEach((element) => {
+//     element.addEventListener('click', markUndelivered)
+// })
 
 async function deleteItem(){
     const addressText = this.parentNode.childNodes[1].innerText
@@ -20,6 +30,24 @@ async function deleteItem(){
         console.log(data)
         location.reload()
     } catch(err) {
+        console.log(err)
+    }
+}
+
+async function markDelivered() {
+    const addressText = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch ('markDelivered', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'addressFromJS': addressText
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    } catch(err){
         console.log(err)
     }
 }
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const { response } = require('express')
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
@@ -26,7 +25,7 @@ app.get('/', async (request, response) => {
 })
 
 app.post('/addHouse', (request, response) => {
-    db.collection('round').insertOne({address: request.body.house, newspaper: request.body.paper, completed: false})
+    db.collection('round').insertOne({address: request.body.house, newspaper: request.body.paper, delivered: false})
     .then(result => {
         console.log('New address added')
         response.redirect('/')
@@ -35,12 +34,29 @@ app.post('/addHouse', (request, response) => {
 })
 
 app.delete('/deleteItem', (request, response) => {
-    db.collection('round').deleteOne({address: request.body.addressFromJS})
+    db.collection('round').deleteOne({houses: request.body.addressFromJS})
     .then(result => {
         console.log('Address Deleted')
         response.json('Address Deleted')
     })
     .catch(error => console.error(error))
+})
+
+app.put('/markDelivered', (request, response) => {
+    db.collection('round').updateOne({address: request.body.addressFromJS},{
+        $set: {
+            delivered: true
+          }
+    },{
+        sort: {_id: -1},
+        upsert: false
+    })
+    .then(result => {
+        console.log('Marked Delivered')
+        response.json('Marked Delivered')
+    })
+    .catch(error => console.error(error))
+
 })
 
 app.listen(process.env.PORT || PORT, () => {
